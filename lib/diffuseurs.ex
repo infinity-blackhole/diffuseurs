081b4e1af26bfe92f1140538dee77fb91fa7eec8
William Phetsinorath <william.phetsinorath@shikanime.studio>
2022-10-08 10:49:39 +0200

    Initial commit

---
+++ lib/diffuseurs.ex
@@ -0,0 +1,18 @@
+defmodule Diffuseurs do
+  @moduledoc """
+  Documentation for `Diffuseurs`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Diffuseurs.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/diffuseurs_test.exs
@@ -0,0 +1,8 @@
+defmodule DiffuseursTest do
+  use ExUnit.Case
+  doctest Diffuseurs
+
+  test "greets the world" do
+    assert Diffuseurs.hello() == :world
+  end
+end
